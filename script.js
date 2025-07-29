e0239ead5f435b85c4f39517b5a34b43c29f247e
AzizShoimqulov <azizshoimqulov74@gmail.com>
2025-07-29 13:51:52 +0500

    asdfgh

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-async function getCountries() {
-  try {
-    const response = await fetch("https://restcountries.com/v3.1/all?fields=name,capital,region");
-    const data = await response.json();
-    console.log(data);
+async function getCountries(){
+    try{
+        const API = await fetch("https://restcountries.com/v3.1/all?fields=name,flags,capital,region")
+        const data = await API.json()
+        console.log(data);
 
-    const container = document.getElementById('product-list');
-    container.innerHTML = ''; // Oldingi "Yuklanmoqda..." matnini tozalash
+        const container = document.getElementById('product-list');
+        container.innerHTML = '';
 
-    data.forEach(country => {
-      const countryDiv = document.createElement('div');
-      countryDiv.className = 'product';
+        data.forEach(country => {
+           const countryDiv = document.createElement('div')
+           countryDiv.className='information'
+           
+        countryDiv.innerHTML= `
+          <div>
+            <img src="${country.flags.png}" alt="${country.flags.alt}" width="80">
+            <h1>${country.name.common}</h1>
+            <h2>${country.capital}</h2>
+            <h3>${country.region}</h3>
+            <h4>${country.name.official}</h4>
+          </div>
+        `;
+        container.appendChild(countryDiv)
+        });
 
-      countryDiv.innerHTML = `
-        <div>
-          <h3>${country.name.common}</h3>
-          <p><strong>Poytaxt:</strong> ${country.capital ? country.capital[0] : "Noma'lum"}</p>
-          <p><strong>Hudud:</strong> ${country.region}</p>
-        </div>
-      `;
-
-      container.appendChild(countryDiv);
-    });
-
-  } catch (error) {
-    console.error("Xatolik:", error);
-  }
+    }catch(error){
+        console.error("Xatolik:", error);
+    }
 }
 
-getCountries();
+getCountries()

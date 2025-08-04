68de3c69686976037aa9dbf764efee746d5c7c63
AzizShoimqulov <azizshoimqulov74@gmail.com>
2025-08-04 10:11:09 +0500

    asda

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-const search = document.getElementById('search')
+const search = document.getElementById('search');
 const regionButtons = document.querySelectorAll('#regions button');
 let allCountries = [];
 
 let savedCountries = localStorage.getItem('countries');
+
 if (savedCountries) {
     allCountries = JSON.parse(savedCountries);
     renderCountries(allCountries);
 } else {
+
     getCountries();
 }
 
-async function getCountries(){
-    try{
+async function getCountries() {
+    try {
         const API = await fetch("https://restcountries.com/v3.1/all?fields=name,flags,capital,region");
         const data = await API.json();
         allCountries = data;
+
+
         localStorage.setItem('countries', JSON.stringify(allCountries));
         renderCountries(allCountries);
-    } catch(error){
+    } catch (error) {
         console.error("Xatolik:", error);
     }
 }
@@ -43,28 +47,36 @@ function renderCountries(countries) {
     });
 }
 
+
 search.addEventListener('input', function () {
     const term = this.value.toLowerCase();
+    localStorage.setItem('searchTerm', term)
+
     const filtered = allCountries.filter(country =>
         country.name.common.toLowerCase().includes(term)
     );
     renderCountries(filtered);
 });
+const savedSearchTerm = localStorage.getItem('searchTerm');
+if (savedSearchTerm) {
+    search.value = savedSearchTerm;
+    const filtered = allCountries.filter(country =>
+        country.name.common.toLowerCase().includes(savedSearchTerm)
+    );
+    renderCountries(filtered);
+}
 
 regionButtons.forEach(button => {
-  button.addEventListener('click', function () {
-    const selectedRegion = this.getAttribute('data-region');
+    button.addEventListener('click', function () {
+        const selectedRegion = this.getAttribute('data-region');
 
-    if (selectedRegion === 'All') {
-      renderCountries(allCountries);
-    } else {
-      const filtered = allCountries.filter(country =>
-        country.region === selectedRegion
-      );
-      renderCountries(filtered);
-    }
-  });
+        if (selectedRegion === 'All') {
+            renderCountries(allCountries);
+        } else {
+            const filtered = allCountries.filter(country =>
+                country.region === selectedRegion
+            );
+            renderCountries(filtered);
+        }
+    });
 });
-
-
-getCountries();
